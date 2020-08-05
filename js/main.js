3a7644d9235457eaa471548179b189a898459d40
Luca <mylince@gmail.com>
2020-08-05 15:50:02 +0200

    Finish

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// $(document).on('scroll', function(){
+//     if ( $(window).scrollTop() > 30) {
+//         $('#container-header').addClass('change-color');
+//     } else {
+//         $('#container-header').removeClass('change-color');
+//     }
+// });
